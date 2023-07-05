b330cf12d30982513a0ae1adf424e7d557fa11c7
arsal uddin <arsaluddin134@gmail.com>
2023-07-05 06:03:58 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const body = require('body-parser');
 const axios = require('axios');
-const apikey = '';
+const EdamamApiClient = require('edamam-api');
 
 const app = express();
 app.use(express.json());
 
+app.get('/',(req,res) => {
+   const apiClient = new EdamamApiClient.RecipeSearchClient({
+    appId : '948beb25',
+    appKey: 'f08715f706df82289cad8488e53129f7',
+   })
+//    const ingridients = req.query.ingridients;
+const ingridients = 'chicken';
 
+   apiClient.search({
+    query: ingridients,
+   }).then((data) =>{
+      console.log(data.recipe.image)
+      res.json(data.hits)
+   })
+})
 
 
 
