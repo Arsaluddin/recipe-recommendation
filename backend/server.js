7dd6c29ff95f58264aeec435031fa4fa96b4dc76
arsal uddin <arsaluddin134@gmail.com>
2023-07-07 10:32:01 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,44 @@ const express = require('express');
 const body = require('body-parser');
 const axios = require('axios');
 const EdamamApiClient = require('edamam-api');
+const cors = require('cors');
+
 
 const app = express();
 app.use(express.json());
 
-app.get('/',(req,res) => {
-   const apiClient = new EdamamApiClient.RecipeSearchClient({
-    appId : '948beb25',
-    appKey: 'f08715f706df82289cad8488e53129f7',
-   })
-//    const ingridients = req.query.ingridients;
-const ingridients = 'chicken';
+app.use(cors())
 
-   apiClient.search({
-    query: ingridients,
-   }).then((data) =>{
-      console.log(data.recipe.image)
-      res.json(data.hits)
-   })
-})
+
+
+app.get('/', (req, res) => {
+    // const { q } = req.query; // Search query from the frontend (optional)
+    const { q } = req.query; 
+    // Make a request to the Edamam API
+    axios.get('https://api.edamam.com/search', {
+      params: {
+        q: q || 'recipe', // Use the search query if provided, otherwise default to 'recipe'
+        app_id: '948beb25',
+        app_key: 'f08715f706df82289cad8488e53129f7',
+      },
+    })
+      .then(response => {
+         
+        const recipes = response.data.hits.map(hit => ({
+          id: hit.recipe.uri,
+          title: hit.recipe.label,
+          calories: hit.recipe.calories,
+          image: hit.recipe.image,
+        }));
+        // console.log(recipes[0])
+        // res.json(recipes[0]);
+        res.send(recipes)
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: 'An error occurred while fetching recipes' });
+      });
+  });
 
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,55 @@
-
+import {useEffect,useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+ 
+const [recipe,setRecipe] = useState([]);
+const [query,setQuery] = useState('');
+
+//  useEffect(()=>{
+//     fetchRecipes();
+//  },[])
+
+ const fetchRecipes = () => {
+  axios
+  .get('http://localhost:9000', {
+    params: {
+      q: query // Pass the query as a parameter in the API request
+    }
+  })
+  .then((res) => {
+    setRecipe(res.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+ }
+
+ const handleSubmit = (e) => {
+     
+     e.preventDefault();
+     fetchRecipes();
+ }
+
+
   return (
     <div className="App">
-     
+      <h1>Recipe Recommandation</h1>
+        <form onSubmit={handleSubmit}>
+          <label>ingridients</label>
+          <input type='text' value={query} onChange={(e) => setQuery(e.target.value)}></input>
+          <button type='submit'>Submit</button>
+        </form>
+        
+          { recipe && recipe.map((data,i) => (
+            <div key={i}>
+              <h3>{data.title}</h3>
+              <h4>{data.calories}</h4>
+            </div>
+          ))}
+        
+        
     </div>
   );
 }
